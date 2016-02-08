a8521c16dda52560cfbfa206cd1fd5361572838a
figmenyaznae <danila2412@gmail.com>
2016-02-08 19:33:33 +0300

    Complete oracle create + formatting
    Added some missing tables in oracle create query and formatted it to
    more convenient form

--- CreateQueries/CreateOracle.sql
+++ CreateQueries/CreateOracle.sql
@@ -1,55 +1,8 @@
 CREATE TABLESPACE HIERARCHYMANAGER DATAFILE 'C:\Users\Daniil\Oracle\app\oracle\oradata\XE\HIERARCHYMANAGER.DBF'
 SIZE 100M REUSE AUTOEXTEND ON NEXT 10M MAXSIZE 200M;
 
-------------------------------------------------------------NodeActions
-create table NodeActions
-(
-  NodeActionID NUMBER PRIMARY KEY,
-  NodeActionName VARCHAR2(20)
-)
-tablespace HIERARCHYMANAGER;
-
-alter table NodeActions modify (NodeActionID NOT NULL enable);
-alter table NodeActions modify (NodeActionName NOT NULL enable);
-
-------------------------------------------------------------FileActions
-create table FileActions
-(
-  FileActionID NUMBER PRIMARY KEY,
-  FileActionName VARCHAR2(20)
-)
-tablespace HIERARCHYMANAGER;
-
-alter table FileActions modify (FileActionID NOT NULL enable);
-alter table FileActions modify (FileActionName NOT NULL enable);
-
----------------------------------------------------------FileExtensions
-CREATE TABLE FileExtensions
-(
-	ExtensionID	NUMBER PRIMARY KEY,
-	Mask		VARCHAR2(12),
-	Extension	VARCHAR2(80),
-    Icon        VARCHAR2(20)
-)
-tablespace HIERARCHYMANAGER;
-
-alter table FileExtensions modify (ExtensionID NOT NULL enable);
-alter table FileExtensions modify (Mask NOT NULL enable);
-alter table FileExtensions modify (Extension NOT NULL enable);
-
-create sequence FileExt_SEQ minvalue 1 maxvalue 100000 increment by 1 start with 1 nocycle;
-
-create or replace trigger FileExt_TRG
-before insert on FileExtensions
-for each row
-begin
-  if :NEW.ExtensionID is null then
-    select FileExt_SEQ.nextval into :NEW.ExtensionID from SYS.DUAL;
-  end if;
-end FileExt_TRG;
-/
-alter trigger FileExt_TRG enable;
-
+-----------------------------------------------------------------------
+---------------------   User operations section   ---------------------
 ------------------------------------------------------------------Users
 CREATE TABLE Users
 (
@@ -78,6 +31,8 @@ end Users_TRG;
 /
 alter trigger Users_TRG enable;
 
+-----------------------------------------------------------------------
+--------------------   Folder operations section   --------------------
 ------------------------------------------------------------------Nodes
 CREATE TABLE Nodes
 (
@@ -138,6 +93,79 @@ end Edges_TRG;
 /
 alter trigger Edges_TRG enable;
 
+------------------------------------------------------------NodeActions
+create table NodeActions
+(
+  NodeActionID NUMBER PRIMARY KEY,
+  NodeActionName VARCHAR2(20)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table NodeActions modify (NodeActionID NOT NULL enable);
+alter table NodeActions modify (NodeActionName NOT NULL enable);
+
+---------------------------------------------------------------NodesLog
+CREATE TABLE NodesLog
+(
+	NodesLogID		NUMBER PRIMARY KEY,
+	NodeActionID	NUMBER,
+	NodeID			NUMBER,
+	UserID			NUMBER,
+	ActionTimeStamp	DATE,
+	CONSTRAINT NodeAction_FK FOREIGN KEY (NodeActionID) REFERENCES NodeActions(NodeActionID),
+	CONSTRAINT NodeLog_FK FOREIGN KEY (NodeID) REFERENCES Nodes(NodeID),
+	CONSTRAINT UserNodeLog_FK FOREIGN KEY (UserID) REFERENCES Users(UserID)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table NodesLog modify (NodesLogID NOT NULL enable);
+alter table NodesLog modify (NodeActionID NOT NULL enable);
+alter table NodesLog modify (NodeID NOT NULL enable);
+alter table NodesLog modify (UserID NOT NULL enable);
+alter table NodesLog modify (ActionTimeStamp NOT NULL enable);
+
+create sequence NodesLog_SEQ minvalue 1 maxvalue 10000000000 increment by 1 start with 1 nocycle;
+
+create or replace trigger NodesLog_TRG
+before insert on NodesLog
+for each row
+begin
+  if :NEW.NodesLogID is null then
+    select NodesLog_SEQ.nextval into :NEW.NodesLogID from SYS.DUAL;
+  end if;
+end NodesLog_TRG;
+/
+alter trigger NodesLog_TRG enable;
+
+-----------------------------------------------------------------------
+---------------------   File operations section   ---------------------
+---------------------------------------------------------FileExtensions
+CREATE TABLE FileExtensions
+(
+	ExtensionID	NUMBER PRIMARY KEY,
+	Mask		VARCHAR2(12),
+	Extension	VARCHAR2(80),
+    Icon        VARCHAR2(20)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table FileExtensions modify (ExtensionID NOT NULL enable);
+alter table FileExtensions modify (Mask NOT NULL enable);
+alter table FileExtensions modify (Extension NOT NULL enable);
+
+create sequence FileExt_SEQ minvalue 1 maxvalue 100000 increment by 1 start with 1 nocycle;
+
+create or replace trigger FileExt_TRG
+before insert on FileExtensions
+for each row
+begin
+  if :NEW.ExtensionID is null then
+    select FileExt_SEQ.nextval into :NEW.ExtensionID from SYS.DUAL;
+  end if;
+end FileExt_TRG;
+/
+alter trigger FileExt_TRG enable;
+
 ------------------------------------------------------------------Files
 CREATE TABLE Files
 (
@@ -180,3 +208,96 @@ tablespace HIERARCHYMANAGER;
 
 alter table NodeFiles modify (NodeID NOT NULL enable);
 alter table NodeFiles modify (FileID NOT NULL enable);
+
+------------------------------------------------------------FileActions
+create table FileActions
+(
+  FileActionID NUMBER PRIMARY KEY,
+  FileActionName VARCHAR2(20)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table FileActions modify (FileActionID NOT NULL enable);
+alter table FileActions modify (FileActionName NOT NULL enable);
+
+---------------------------------------------------------------FilesLog
+CREATE TABLE FilesLog
+(
+	FilesLogID		NUMBER PRIMARY KEY,
+	FileActionID	NUMBER,
+	FileID			NUMBER,
+	UserID			NUMBER,
+	ActionTimeStamp	DATE,
+	CONSTRAINT FileAction_FK FOREIGN KEY (FileActionID) REFERENCES FileActions(FileActionID),
+	CONSTRAINT FileLog_FK FOREIGN KEY (FileID) REFERENCES Files(FileID),
+	CONSTRAINT UserFileLog_FK FOREIGN KEY (UserID) REFERENCES Users(UserID)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table FilesLog modify (FilesLogID NOT NULL enable);
+alter table FilesLog modify (FileActionID NOT NULL enable);
+alter table FilesLog modify (FileID NOT NULL enable);
+alter table FilesLog modify (UserID NOT NULL enable);
+alter table FilesLog modify (ActionTimeStamp NOT NULL enable);
+
+create sequence FilesLog_SEQ minvalue 1 maxvalue 10000000000 increment by 1 start with 1 nocycle;
+
+create or replace trigger FilesLog_TRG
+before insert on FilesLog
+for each row
+begin
+  if :NEW.FilesLogID is null then
+    select FilesLog_SEQ.nextval into :NEW.FilesLogID from SYS.DUAL;
+  end if;
+end FilesLog_TRG;
+/
+alter trigger FilesLog_TRG enable;
+
+-----------------------------------------------------------------------
+-------------------   Extended operations section   -------------------
+----------------------------------------------------------------Ratings
+
+CREATE TABLE Ratings
+(
+	UserID	NUMBER,
+	FileID	NUMBER,
+	Value	NUMBER,
+	PRIMARY KEY (UserID, FileID),
+	CONSTRAINT UserRating_FK FOREIGN KEY (UserID) REFERENCES Users(UserID),
+	CONSTRAINT FileRating_FK FOREIGN KEY (FileID) REFERENCES Files(FileID)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table Ratings modify (UserID NOT NULL enable);
+alter table Ratings modify (FileID NOT NULL enable);
+alter table Ratings modify (Value NOT NULL enable);
+
+---------------------------------------------------------------Comments
+CREATE TABLE Comments
+(
+	CommentID	NUMBER PRIMARY KEY,
+	UserID		NUMBER,
+	FileID		NUMBER,
+	CommentText	VARCHAR2(800),
+	CONSTRAINT UserComment_FK FOREIGN KEY (UserID) REFERENCES Users(UserID),
+	CONSTRAINT FileComment_FK FOREIGN KEY (FileID) REFERENCES Files(FileID)
+)
+tablespace HIERARCHYMANAGER;
+
+alter table Comments modify (CommentID NOT NULL enable);
+alter table Comments modify (UserID NOT NULL enable);
+alter table Comments modify (FileID NOT NULL enable);
+alter table Comments modify (CommentText NOT NULL enable);
+
+create sequence Comments_SEQ minvalue 1 maxvalue 10000000000 increment by 1 start with 1 nocycle;
+
+create or replace trigger Comments_TRG
+before insert on Comments
+for each row
+begin
+  if :NEW.CommentID is null then
+    select Comments_SEQ.nextval into :NEW.CommentID from SYS.DUAL;
+  end if;
+end Comments_TRG;
+/
+alter trigger FilesLog_TRG enable;
